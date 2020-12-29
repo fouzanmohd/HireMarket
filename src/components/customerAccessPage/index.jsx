792d991846ac4bf122ba9dfcb3f3993cf3d2be11
Muhammed Fouzan <fouzanpharmd@gmail.com>
2020-12-29 22:16:30 +0530

    added signup and signin page

--- src/components/customerAccessPage/index.jsx
+++ src/components/customerAccessPage/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Footer } from '../footer'
 import { NavBar } from '../Navbar'
 import { InnerPageContainer, PageContainer } from '../pageContainer'
+import { AccountBox } from './../accountBox/index';
 
 export const CustomerAccessPage = (props) => {
     return <PageContainer>
         <NavBar/>
         <InnerPageContainer>
-            Boom
+            <AccountBox/>
         </InnerPageContainer>
         <Footer/>
     </PageContainer>
